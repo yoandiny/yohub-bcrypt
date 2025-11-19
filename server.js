8af21966bd1add962f9908fbdf426ed8b37c3f72
Yoan <yoandiny14@gmail.com>
2025-11-19 23:11:41 +0300

    Update server.js to change default port from 5002 to 5005

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/check', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5005;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
